1440a8fbd2d89886930e3fd41b7c7fd5bc5efb38
jimngmerkle <126862696+jimngmerkle@users.noreply.github.com>
2024-11-26 10:47:47 +0100

    main update

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
 import App from './App';
 import './index.css';
 import { AuthProvider } from "./utils/AuthContext"; // Ensure the path is correct
 
-
-// Here the App component is being rendered in the browser and we have most of the functionality in the App component
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <Router> {/* Wrap everything inside Router */}
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Router>
   </React.StrictMode>
 );
