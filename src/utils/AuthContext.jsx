e27417ffb6485372280764b126123c25719a4f20
jimngmerkle <126862696+jimngmerkle@users.noreply.github.com>
2024-11-26 10:28:46 +0100

    auth update

--- src/utils/AuthContext.jsx
+++ src/utils/AuthContext.jsx
@@ -1,7 +1,8 @@
 import React, { createContext, useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-// Create AuthContext
 const AuthContext = createContext();
+const navigate = useNavigate(); 
 
 // Create a provider to wrap the app and provide global state
 export const AuthProvider = ({ children }) => {
